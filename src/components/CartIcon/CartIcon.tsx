d5adcc7a8614478273e04a9221581237a8886b26
raunak299 <iamraunak299@gmail.com>
2024-08-14 11:43:08 +0530

    cart icon ui

---
+++ src/components/CartIcon/CartIcon.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import styles from "./CartIcon.module.css";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+
+function CartIcon(props: { count: number }): JSX.Element {
+  return (
+    <Link href="/cart">
+      <div className={styles.cart}>
+        {props.count > 0 && (
+          <div className={styles.cartCount}>{props.count}</div>
+        )}
+        <ShoppingCartOutlinedIcon />
+      </div>
+    </Link>
+  );
+}
+
+export default CartIcon;

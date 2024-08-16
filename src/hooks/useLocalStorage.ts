f53c60fd3779b8507e475ec857459a824aac5080
raunak299 <iamraunak299@gmail.com>
2024-08-16 17:51:40 +0530

    use localstorage js doc

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -3,6 +3,23 @@ export type UseLocalStorageProps<T> = {
   defaultValue?: T | null;
 };
 
+/**
+ * Custom hook for interacting with local storage.
+ *
+ * This hook provides functions to get, set, and remove items from local storage. It uses the provided key to
+ * identify the item and optionally allows specifying a default value if the item is not found.
+ *
+ * @template T - The type of the value stored in local storage.
+ * @param {UseLocalStorageProps<T>} props - The props for initializing the hook.
+ * @param {string} props.key - The key used to store and retrieve data from local storage.
+ * @param {T | null} [props.defaultValue] - An optional default value to return if the key does not exist or if there is an error.
+ *
+ * @returns {Object} An object with the following methods:
+ * @returns {() => T | null} getFromLocalStorage - A function that retrieves the value from local storage. Returns `null` if the key is not found or if there is an error.
+ * @returns {(value: T) => void} setLocalStorage - A function that sets the value in local storage.
+ * @returns {() => void} removeLocalStorage - A function that removes the value associated with the key from local storage.
+ */
+
 const useLocalStorage = <T>(props: UseLocalStorageProps<T>) => {
   const { key, defaultValue = null } = props;
 

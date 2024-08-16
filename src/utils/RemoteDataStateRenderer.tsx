3cc158917136e6e4d98a6451c49c2151382a1f74
raunak299 <iamraunak299@gmail.com>
2024-08-16 17:25:08 +0530

    remotedatastaterenderer modified
    - added custom loader to loading msg
    - added cusotm error to err msg

--- src/utils/RemoteDataStateRenderer.tsx
+++ src/utils/RemoteDataStateRenderer.tsx
@@ -1,29 +1,25 @@
-import {RemoteDataStateRendererProps} from '@/types';
-
+import { RemoteDataStateRendererProps } from "@/types";
+import { Loader,Error } from "@/ui";
 
 function RemoteDataStateRenderer<T>({
-    remoteDataState,
-    renderSuccess,
-    renderInit = () => <p>Initializing...</p>,
-    renderLoading = (message) => <p>{message || 'Loading...'}</p>,
-    renderError = (message) => (
-      <div>
-        {message && <p>{message}</p>}
-      </div>
-    ),
-  }: RemoteDataStateRendererProps<T>){
-    switch (remoteDataState.status) {
-        case 'init':
-          return <>{renderInit()}</>;
-        case 'loading':
-          return <>{renderLoading(remoteDataState.message)}</>;
-        case 'error':
-          return <>{renderError(remoteDataState.message)}</>;
-        case 'success':
-          return <>{renderSuccess(remoteDataState.data)}</>;
-        default:
-          return null;
-      }
+  remoteDataState,
+  renderSuccess,
+  renderInit = () => <p>Initializing...</p>,
+  renderLoading = (message) => <Loader message={message || "Loading ..."} />,
+  renderError = (message) => <Error message={message || "Something went wrong"} />,
+}: RemoteDataStateRendererProps<T>) {
+  switch (remoteDataState.status) {
+    case "init":
+      return <>{renderInit()}</>;
+    case "loading":
+      return <>{renderLoading(remoteDataState.message)}</>;
+    case "error":
+      return <>{renderError(remoteDataState.message)}</>;
+    case "success":
+      return <>{renderSuccess(remoteDataState.data)}</>;
+    default:
+      return null;
+  }
 }
 
 export default RemoteDataStateRenderer;

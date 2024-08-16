ce7363c8741a159695fd3a80fbec26f50733e6ab
raunak299 <iamraunak299@gmail.com>
2024-08-16 17:41:13 +0530

    user profile page added with children

--- src/app/userprofile/page.tsx
+++ src/app/userprofile/page.tsx
@@ -1,8 +1,10 @@
-'use client';
+"use client";
 import { ProtectedRoute, UserProfile } from "@/components";
 
 export default function userProfile(): JSX.Element {
   return (
-    <ProtectedRoute children={<UserProfile/>}/>
+    <ProtectedRoute>
+      <UserProfile />
+    </ProtectedRoute>
   );
 }

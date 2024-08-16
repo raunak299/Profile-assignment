ca86b9a23e27178953483a688bf32facb6152992
raunak299 <iamraunak299@gmail.com>
2024-08-16 17:35:15 +0530

    button modified
    - loading state
    - disabled state

--- src/ui/Button/Button.tsx
+++ src/ui/Button/Button.tsx
@@ -1,17 +1,37 @@
+import { CircularProgress } from "@mui/material";
 import styles from "./Button.module.css";
 import { ButtonProps } from "./types";
 
-
 function Button(props: ButtonProps) {
-  const { label, onClick, size = "fullWidth", color = "primary", icon } = props;
-  const buttonClass = `${styles.button} ${styles[`button-${color}`]} ${styles[`button-${size}`]}`;
+  const {
+    label,
+    onClick,
+    size = "fullWidth",
+    color = "primary",
+    icon,
+    disabled = false,
+    type,
+    loading = false,
+  } = props;
+
+  const buttonClass = `${styles.button} ${styles[`button-${color}`]} ${
+    styles[`button-${size}`]
+  } ${loading ? styles["button-loading"] : ""}`;
+
+  const loadingSpinner = (
+    <CircularProgress className={styles[`loadingSpinner-${color}`]} size="1rem"/>
+  );
+
+  const buttonIcon = icon ? icon : loading ? loadingSpinner : null;
 
   return (
     <button
       onClick={onClick && onClick}
       className={buttonClass}
+      disabled={disabled}
+      type={type}
     >
-      {icon && <span className="material-icons">{icon}</span>}
+      {buttonIcon && <span className={styles['button-icons']}>{buttonIcon}</span>}
       {label}
     </button>
   );
--- src/ui/Button/types/index.ts
+++ src/ui/Button/types/index.ts
@@ -1,7 +1,12 @@
+import { ReactNode } from "react";
+
 export type ButtonProps = {
-  label: string;
+  label: ReactNode;
   onClick?: () => void;
   size?: "small" | "medium" | "large" | "fullWidth";
   color?: "primary" | "secondary";
   icon?: string;
+  disabled?: boolean;
+  type?: "submit";
+  loading?: boolean;
 };

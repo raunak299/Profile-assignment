62528ad824827a480a5fd4b11f6c4d50e97c1142
raunak299 <iamraunak299@gmail.com>
2024-08-16 17:34:50 +0530

    added support of reactnode to rmeotedatastate

--- src/types/remoteDataState.ts
+++ src/types/remoteDataState.ts
@@ -1,13 +1,15 @@
+import { ReactNode } from "react";
+
 export type RemoteDataState<T> =
   | { status: "init" }
-  | { status: "loading"; message?: string }
-  | { status: "error"; message?: string }
-  | { status: "success"; data: T };
+  | { status: "loading"; message: ReactNode }
+  | { status: "error"; message: ReactNode }
+  | { status: "success"; message?: ReactNode; data: T };
 
 export type RemoteDataStateRendererProps<T> = {
   remoteDataState: RemoteDataState<T>;
   renderSuccess: (data: T) => React.ReactNode;
   renderInit?: () => React.ReactNode;
-  renderLoading?: (message?: string) => React.ReactNode;
-  renderError?: (message?: string) => React.ReactNode;
+  renderLoading?: (message?: ReactNode) => React.ReactNode;
+  renderError?: (message?: ReactNode) => React.ReactNode;
 };

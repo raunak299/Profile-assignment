abc6952fa2adcb029b66c2130e924f39b07773a2
raunak299 <iamraunak299@gmail.com>
2024-08-16 17:47:59 +0530

    jsdoc for RemoteDataState

--- src/types/remoteDataState.ts
+++ src/types/remoteDataState.ts
@@ -1,5 +1,18 @@
 import { ReactNode } from "react";
 
+/**
+ * Represents the different states of remote data.
+ *
+ * The `RemoteDataState` type is a union of objects that represent various states of fetching data from a remote source.
+ * It can be in one of four states:
+ * - `init`: Indicates the data is in the initialization phase.
+ * - `loading`: Indicates the data is being loaded. Includes an optional message.
+ * - `error`: Indicates an error occurred while fetching the data. Includes an optional error message.
+ * - `success`: Indicates the data was successfully fetched. Includes the data and an optional success message.
+ *
+ * @template T - The type of the data being fetched.
+ */
+
 export type RemoteDataState<T> =
   | { status: "init" }
   | { status: "loading"; message: ReactNode }

25631b8a1d8ffd15483b5e4d5dad0d2c2b18e308
raunak299 <iamraunak299@gmail.com>
2024-08-16 17:37:40 +0530

    cart page added

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -2,17 +2,31 @@ import PageHeader from "../PageHeader/PageHeader";
 import styles from "./Cart.module.css";
 import CartProductCard from "./CartProductCard/CartProductCard";
 import useCartContext from "@/context/hooks/useCartContext";
+import CartSummary from "./CartSummary/CartSummary";
+import ProceedToCheckoutButton from "./ProceedToCheckoutButton/ProceedToCheckoutButton";
+import EmptyMessage from "./EmptyMessage/EmptyMessage";
 
 function Cart() {
   const { products } = useCartContext();
 
   return (
-    <section>
+    <section className={styles.cartRoot}>
       <PageHeader label="Cart" />
-      <section className={styles.cartProductContainer}>
-        {products.map((product) => {
-          return <CartProductCard product={product} key={product.id} />;
-        })}
+      <section className={styles.cartBody}>
+        {products.length > 0 && (
+          <section className={styles.cartProductContainer}>
+            {products.map((product) => {
+              return <CartProductCard product={product} key={product.id} />;
+            })}
+          </section>
+        )}
+        {products.length === 0 && (
+          <EmptyMessage message="... Empty"/>
+        )}
+        <section className={styles.summarySection}>
+          <CartSummary />
+          <ProceedToCheckoutButton noOfProductsInCart={products.length} />
+        </section>
       </section>
     </section>
   );

e917c2af22727a9d8961b97c34da4a65682a018b
raunak299 <iamraunak299@gmail.com>
2024-08-16 17:48:57 +0530

    jsdoc for getTotalCartAmount

--- src/utils/cartSummary.utils.ts
+++ src/utils/cartSummary.utils.ts
@@ -8,6 +8,21 @@ export function getTotalProductPrice(products: ProductWithQuantity[]): number {
   );
 }
 
+/**
+ * Calculates the total amount for the cart, including product price, shipping charge, tax, and discount.
+ *
+ * This function computes the total cart amount by applying the discount to the total product price,
+ * then adding the shipping and tax charges. The result is rounded up to the nearest whole number
+ * and ensures it is not negative.
+ *
+ * @param {number} totalProductPrice - The total price of the products in the cart before any discounts or additional charges.
+ * @param {number} shippingCharge - The shipping cost to be added to the total cart amount.
+ * @param {number} taxCharge - The tax amount to be added to the total cart amount.
+ * @param {number} [discountPercentage=0] - The discount percentage to apply to the total product price. Defaults to 0 if not provided.
+ *
+ * @returns {number} The total amount for the cart after applying the discount, adding the shipping and tax charges.
+ */
+
 export function getTotalCartAmount(
   totalProductPrice: number,
   shippingCharge: number,

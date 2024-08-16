dea56452a50ce65419498f566a17475c28f1a805
raunak299 <iamraunak299@gmail.com>
2024-08-16 17:40:23 +0530

    payment contant file added

---
+++ src/constants/payment.ts
@@ -0,0 +1,16 @@
+export const SHIPPING_CHARGE = 5;
+export const TAX_CHARGE = 70;
+export const DISCOUNT_CODES = [
+  {
+    label: "FLAT 10%",
+    discount: 10,
+  },
+  {
+    label: "FLAT 30%",
+    discount: 30,
+  },
+  {
+    label: "FLAT 50%",
+    discount: 50,
+  },
+];

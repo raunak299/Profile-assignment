ff2c19ce99c04610f0b234569abd0a98c31ceb87
raunak299 <iamraunak299@gmail.com>
2024-08-16 17:41:44 +0530

    authentication added to add to cart btn

--- src/components/Products/ProductCard/ProductCard.tsx
+++ src/components/Products/ProductCard/ProductCard.tsx
@@ -3,6 +3,7 @@ import styles from "./ProductCard.module.css";
 import Image from "next/image";
 import { Button } from "@/ui";
 import useCartContext from "@/context/hooks/useCartContext";
+import AddToCartButton from "@/components/Cart/AddToCartButton/AddToCartButton";
 
 export default function ProductCard(props: { product: Product }): JSX.Element {
   const { product } = props;
@@ -31,9 +32,9 @@ export default function ProductCard(props: { product: Product }): JSX.Element {
       />
       <div className={styles.cardBody}>
         <h2 className={styles.title}>{product.attributes.title}</h2>
-        <h3 className={styles.price}>{`${product.attributes.price}`}</h3>
+        <h3 className={styles.price}>{`$${product.attributes.price}`}</h3>
       </div>
-      <Button
+      <AddToCartButton
         label={"Add to cart"}
         size="fullWidth"
         onClick={() => addProductToCartHandler(product)}
